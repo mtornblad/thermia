4b5739d1e2f3a5325cd41f9dcf1565d0097207e2
Mattias Tornblad <mattias.tornblad@gmail.com>
2025-11-24 08:40:03 +0100

    updates

--- firmware/Thermia_Bridge/Thermia_Bridge.ino
+++ firmware/Thermia_Bridge/Thermia_Bridge.ino
@@ -1,12 +1,12 @@
 /*
  * Thermia I2C <-> Modbus Bridge
  * Hårdvara: Seeed Studio XIAO RA4M1
+ * Bibliotek: Modbus-Master-Slave-for-Arduino (smarmengol)
  */
 
 // ============================================================================
 // *** KONFIGURATION AV SCENARIO ***
 // Avkommentera raden nedan om du kör Scenario 2 (Piggyback med C6 direkt på).
-// Låt den vara kommenterad om du kör Scenario 1 eller 3 (RS485-modul).
 // ============================================================================
 
 // #define CONFIG_PIGGYBACK_MODE 
@@ -16,41 +16,44 @@
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <ModbusRTU.h>
+#include <ModbusRtu.h>   // Smarmengol biblioteket
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
 
 // --- Konfiguration beroende på Scenario ---
 #ifdef CONFIG_PIGGYBACK_MODE
-  // --- SCENARIO 2: PIGGYBACK ---
-  // Vi använder Hardware Serial (Serial1) på D6/D7.
-  // Fördel: Bättre prestanda och möjliggör direkt stacking av korten.
+  // SCENARIO 2: PIGGYBACK (Hardware Serial1)
   #define COMM_SERIAL Serial1
   #define COMM_BAUD 9600
-  #define RS485_DIR_PIN -1 // Ingen flödeskontroll vid direkt UART
-  
+  #define RS485_DIR_PIN 0 // Används ej, sätt till 0
 #else
-  // --- SCENARIO 1 & 3: RS485 MODUL ---
-  // Vi måste använda D4/D5 pga hur Seeed-skölden är byggd.
-  // Eftersom RA4M1 inte har standard UART där kör vi SoftwareSerial.
+  // SCENARIO 1 & 3: RS485 MODUL (SoftwareSerial)
   SoftwareSerial rs485Serial(D4, D5); // RX=D4, TX=D5
   #define COMM_SERIAL rs485Serial
   #define COMM_BAUD 9600
-  #define RS485_DIR_PIN D2 // Skölden kräver styrning på D2
+  #define RS485_DIR_PIN D2 
 #endif
+
+// --- Debug Serial ---
+#define DEBUG_SERIAL Serial
 
 // --- Gemensam Hårdvara ---
 #define I2C_SLAVE_ADDR 0x2E   
 #define MODBUS_SLAVE_ID 10    
-#define ONE_WIRE_BUS D0        // Skruvplint "INT" på RS485-kortet
+#define ONE_WIRE_BUS D0        
 
 // --- Minnesmappning ---
 #define TOTAL_REGS 1020 
 #define SENSOR_START_REG 1000
 #define MAX_SENSORS 10 
 
-// --- Globala Objekt ---
-ModbusRTU mb;
+// --- Globala Data ---
+// Detta är "minnet" som Modbus och I2C delar på
+uint16_t au16data[TOTAL_REGS];
+
+// Modbus Objekt (ID, Port, TxEnablePin)
+Modbus slave(MODBUS_SLAVE_ID, COMM_SERIAL, RS485_DIR_PIN);
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
@@ -68,41 +71,36 @@ SavedSensor knownSensors[MAX_SENSORS];
 void setup() {
   DEBUG_SERIAL.begin(115200);
   
-  // Initiera Kommunikation (Hårdvara eller Mjukvara beroende på #define)
+  // Initiera Modbus Serial
   COMM_SERIAL.begin(COMM_BAUD);
   
-  // Konfigurera Flow Control (om RS485)
-  if (RS485_DIR_PIN != -1) {
-     pinMode(RS485_DIR_PIN, OUTPUT);
-     digitalWrite(RS485_DIR_PIN, LOW); // Lyssna
-  }
-  
-  mb.begin(&COMM_SERIAL, RS485_DIR_PIN);
-  mb.slave(MODBUS_SLAVE_ID);
-  mb.addHreg(0, 0, TOTAL_REGS); 
+  // Starta Modbus (Smarmengol biblioteket startar via konstruktorn och begin())
+  slave.start();
 
   // Initiera OneWire
   sensors.begin();
   
-  // Initiera I2C Slav på D8/D9 (Wire1) - 5V Toleranta
-  Wire1.begin(I2C_SLAVE_ADDR);
-  Wire1.onReceive(receiveEvent);
-  Wire1.onRequest(requestEvent);
+  // Initiera I2C Slav på D8/D9 (Wire)
+  Wire.setSDA(D9);
+  Wire.setSCL(D8);
+  Wire.begin(I2C_SLAVE_ADDR);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 
   DEBUG_SERIAL.println("--- Thermia Bridge Started ---");
   #ifdef CONFIG_PIGGYBACK_MODE
-    DEBUG_SERIAL.println("Mode: Piggyback (Hardware Serial1 on D6/D7)");
+    DEBUG_SERIAL.println("Mode: Piggyback");
   #else
-    DEBUG_SERIAL.println("Mode: RS485 (SoftwareSerial on D4/D5 + Ctrl D2)");
+    DEBUG_SERIAL.println("Mode: RS485 (Ctrl D2)");
   #endif
-  DEBUG_SERIAL.println("I2C: D9(SDA)/D8(SCL)");
-  DEBUG_SERIAL.println("OneWire: D0");
   
   initSensors();
 }
 
 void loop() {
-  mb.task();
+  // Modbus Poll: Vi skickar med vår data-array
+  slave.poll(au16data, TOTAL_REGS);
+  
   yield();
   handleTemperature();
 }
@@ -110,7 +108,7 @@ void loop() {
 // --- Sensorlogik ---
 
 void initSensors() {
-  DEBUG_SERIAL.println("Scanning OneWire bus on Pin D0...");
+  DEBUG_SERIAL.println("Scanning OneWire...");
   
   for (int i = 0; i < MAX_SENSORS; i++) {
     int eepromAddr = i * 8;
@@ -171,7 +169,8 @@ void handleTemperature() {
         float tempC = sensors.getTempC(knownSensors[i].addr);
         if(tempC != DEVICE_DISCONNECTED_C) {
           int16_t storedTemp = (int16_t)(tempC * 100);
-          mb.Hreg(SENSOR_START_REG + i, storedTemp);
+          // Uppdatera arrayen direkt
+          au16data[SENSOR_START_REG + i] = storedTemp;
         }
       }
     }
@@ -201,24 +200,26 @@ void printAddress(DeviceAddress deviceAddress) {
   }
 }
 
-// --- I2C Callbacks (Wire1) ---
+// --- I2C Callbacks ---
 
 void receiveEvent(int howMany) {
   if (howMany == 0) return;
-  i2cIndex = Wire1.read();
-  while (Wire1.available()) {
-    uint8_t val = Wire1.read();
+  i2cIndex = Wire.read();
+  while (Wire.available()) {
+    uint8_t val = Wire.read();
     if (i2cIndex < 256) {
       int8_t signedVal = (int8_t)val;
-      mb.Hreg(i2cIndex, (int16_t)signedVal);
+      // Skriv direkt till den globala arrayen
+      au16data[i2cIndex] = (int16_t)signedVal;
       i2cIndex++;
-    } else { Wire1.read(); }
+    } else { Wire.read(); }
   }
 }
 
 void requestEvent() {
   if (i2cIndex < 256) {
-    uint16_t val16 = mb.Hreg(i2cIndex);
-    Wire1.write((uint8_t)val16);
-  } else { Wire1.write(0x00); }
+    // Läs direkt från den globala arrayen
+    uint16_t val16 = au16data[i2cIndex];
+    Wire.write((uint8_t)val16);
+  } else { Wire.write(0x00); }
 }
